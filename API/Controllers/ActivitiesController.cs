b588f28bfb5543467e3cb85c6b260d8f4b112559
fathi3060 <fathi3060@gmail.com>
2020-12-24 22:22:28 +0100

    gestion des activités : edit, delete

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -18,22 +18,40 @@ namespace API.Controllers
             _mediator = mediator;
         }
 
+        //méthode de recherche de toutes les activités
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> List()
         {
             return await _mediator.Send(new List.Query());
         }
 
+        //méthode de recherche d'une activité en fonction de l'id
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> Details(Guid id)
         {
             return await _mediator.Send(new Details.Query { Id = id });
         }
 
+        //méthode de creation d'une activité (appel de la classe Create dans "Application")
         [HttpPost]
         public async Task<ActionResult<Unit>> Create(Create.Command command)
         {
             return await _mediator.Send(command);
+        }
+
+        //méthode de modification d'une activité (appel de la classe Edit dans "Application")
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Unit>> Edit(Guid id, Edit.Command command)
+        {
+            command.Id = id;
+            return await _mediator.Send(command);
+        }
+
+        //méthode de suppression d'une activité (appel de la classe Delete dans "Application")
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Unit>> Delete(Guid id)
+        {
+            return await _mediator.Send(new Delete.Command { Id = id });
         }
 
     }
